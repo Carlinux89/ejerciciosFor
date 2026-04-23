93c65df012b94b8618591fc13cfd6ed70c3490f4
Carlinux89 <carlosj_1912_17@hotmail.com>
2026-04-22 22:24:33 -0500

    archivos iniciales ejercicios FOR

---
+++ ejercicio.js
@@ -0,0 +1,73 @@
+function listarNumeros() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i)
+    }
+}
+
+
+function listarNumerosReversa() {
+    for (let i = 10; i > 0; i--) {
+        console.log(i)
+    }
+}
+
+function listarPares() {
+    for (let i = 0; i < 20; i += 2) {
+        console.log(i)
+    }
+}
+
+function listarImpares() {
+    for (let i = 0; i < 15; i++) {
+        if (i % 2 != 0) {
+            console.log(i)
+        }
+
+    }
+}
+
+function sumarNumeros() {
+    let suma = 0;
+    for (let i = 0; i < 10; i++) {
+        suma += i;
+        console.log(suma)
+    }
+}
+
+function mostrarEnTxt(idTxt) {
+    let componente = document.getElementById(idTxt);
+    return componente.value;
+}
+
+function limpiar() {
+    document.getElementById("txtEjercicio").value = "";
+    console.clear();
+}
+
+//funcion Principal que ejecuta boton HTML
+function ejecutar() {
+    console.clear();
+    let ejercicio = parseInt(mostrarEnTxt("txtEjercicio"));
+    if (ejercicio == 1) {
+        listarNumeros()
+    } else if (ejercicio == 2) {
+        listarNumerosReversa()
+    } else if (ejercicio == 3) {
+        listarPares()
+    } else if (ejercicio == 4) {
+        listarImpares()
+    } else if (ejercicio == 5) {
+        sumarNumeros()
+    }
+}
+
+
+// function ejecutar(numEjercicio) {
+//     if (numEjercicio == 1) {
+//         listarNumeros()
+//     } else if (numEjercicio == 2) {
+//         listarNumerosReversa()
+//     } else if (numEjercicio == 3) {
+//         listarPares()
+//     }
+// }
